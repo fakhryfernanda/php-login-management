44f734b74544ab7973b00c8539a368d81b874114
fakhryfernanda <mff023@gmail.com>
2022-12-06 16:14:02 +0700

    Create index view

--- public/index.php
+++ public/index.php
@@ -6,8 +6,6 @@ use fakhryfernanda\PHP\Login\Management\App\Router;
 use fakhryfernanda\PHP\Login\Management\Controller\HomeController;
 
 Router::add('GET', '/', HomeController::class, 'index');
-Router::add('GET', '/hello', HomeController::class, 'hello');
-Router::add('GET', '/world', HomeController::class, 'world');
-Router::add('GET', '/about', HomeController::class, 'about');
+
 
 Router::run(); 
