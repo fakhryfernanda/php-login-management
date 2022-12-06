defdceef46cecde0f9db935d66abca98662fdda2
fakhryfernanda <mff023@gmail.com>
2022-12-06 16:47:58 +0700

    Create Domain for User Class

---
+++ app/Domain/User.php
@@ -0,0 +1,12 @@
+<?php
+
+// Folder Domain adalah model representasi tabel di database
+
+namespace fakhryfernanda\PHP\Login\Management\Domain;
+
+class User
+{
+    public string $id;
+    public string $username;
+    public string $password;
+}
